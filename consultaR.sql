1c6810cd0e55817252b344f70c646991ef51adb2
alexandremartinelli11 <alexandremartinelli11@gmail.com>
2025-04-04 11:31:07 -0300

    Db Mercadinho 🎭🐧
    Co-Authored-By: Z-Hugo-Ferraz <169057167+Z-Hugo-Ferraz@users.noreply.github.com>
    Co-Authored-By: akemi <akemi.mullis@gmail.com>
    Co-Authored-By: gabrielccr <163345470+gabrielccr-555@users.noreply.github.com>
    Co-Authored-By: tigasparzin <169062642+tigasparzin@users.noreply.github.com>

--- consultaR.sql
+++ consultaR.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE IF NOT EXISTS mercadinho DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_0900_ai_ci;
-
 -- Todos os deltas estão em segundos
 
 USE mercadinho;
@@ -13,33 +11,12 @@ USE mercadinho;
 -- topic v3/espm/devices/presence07/up
 -- topic v3/espm/devices/presence08/up
 -- { "end_device_ids": { "device_id": "presence01" }, "uplink_message": { "rx_metadata": [{ "timestamp": 2040934975 }], "decoded_payload": { "battery": 99, "occupancy": "vacant" } } }
-CREATE TABLE presenca (
-  id bigint NOT NULL,
-  data datetime NOT NULL,
-  id_sensor tinyint NOT NULL,
-  delta int NOT NULL,
-  bateria tinyint NOT NULL,
-  ocupado tinyint NOT NULL,
-  PRIMARY KEY (id),
-  KEY presenca_data_id_sensor (data, id_sensor),
-  KEY presenca_id_sensor (id_sensor)
-);
+
 
 -- topic v3/espm/devices/passage01/up
 -- topic v3/espm/devices/passage02/up
 -- { "end_device_ids": { "device_id": "passage01" }, "uplink_message": { "rx_metadata": [{ "timestamp": 2040934975 }], "decoded_payload": { "battery": 0, "period_in": 0, "period_out": 0 } } }
-CREATE TABLE passagem (
-  id bigint NOT NULL,
-  data datetime NOT NULL,
-  id_sensor tinyint NOT NULL,
-  delta int NOT NULL,
-  bateria tinyint NOT NULL,
-  entrada int NOT NULL,
-  saida int NOT NULL,
-  PRIMARY KEY (id),
-  KEY passagem_data_id_sensor (data, id_sensor),
-  KEY passagem_id_sensor (id_sensor)
-);
+
 
 -- Query para monitorar em tempo real
 (select id_sensor, ocupado, time_to_sec(timediff(now(), data)) delta_agora from presenca where id_sensor = 1 order by id desc limit 1)
